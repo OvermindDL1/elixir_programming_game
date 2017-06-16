d2b89a33f0af74675389f861a12494c73cd5021f
OvermindDL1 <overminddl1@gmail.com>
2017-06-16 17:36:53 -0600

    Testing some logging in tests...

--- test/elixir_programming_game/actor_test.exs
+++ test/elixir_programming_game/actor_test.exs
@@ -1,5 +1,6 @@
 defmodule ElixirProgrammingGameTest.ActorTest do
   use ExUnit.Case, async: false # true
+  # import ExUnit.CaptureLog
   doctest ElixirProgrammingGame.Actor
   alias ElixirProgrammingGame.Actor
 
@@ -36,4 +37,17 @@ defmodule ElixirProgrammingGameTest.ActorTest do
     assert_receive({:actor_state, :request, %{id: ^id}=state})
     assert %{} = state
   end
+
+  #ExUnit is not capture the log...
+  # test "test cmds - log", %{actor: actor} do
+  #   {:ok, actor} = Actor.start_link(save_to_pid: self())
+  #   Actor.set_event_code(actor, "user1", """
+  #     cmd = Actor.log("Tester")
+  #     {cmd, state}
+  #   """)
+  #   assert capture_log(fn ->
+  #     Actor.send_event(actor, "user1", %{})
+  #     id = Actor.get_id(actor) # Synchronize
+  #   end) =~ "Blah"
+  # end
 end
